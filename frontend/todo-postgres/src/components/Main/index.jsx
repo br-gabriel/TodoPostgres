e28301296a864aa0721bda38fde5be68a424537f
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-07-18 16:08:31 -0300

    main component

---
+++ frontend/todo-postgres/src/components/Main/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles"
+
+export function Main() {
+    return (
+        <Container>
+            <h1>Minhas tarefas</h1>
+        </Container>
+    )
+}
---
+++ frontend/todo-postgres/src/components/Main/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.main `
+    max-width: 1120px;
+    margin: -10rem 2rem;
+    background-color: var(--shape);
+    border-radius: 1rem;
+
+    box-shadow: 4px 4px 8px rgba(39, 63, 172, 0.33);
+`
