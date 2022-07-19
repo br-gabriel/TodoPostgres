9899e132a582bba62073ce2b6387b7200434a57a
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-07-18 21:52:44 -0300

    comments removed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,12 @@
 const cors = require("cors")
 const express = require("express");
 const todosRoutes = require("./routes/todos.routes");
-//const corsConfig = require("./server.config")
 
 const app = express();
 
 app.use(express.json());
 app.use(todosRoutes);
 app.use(cors());
-//app.use(corsConfig);
 
 app.get("/test", (req, res) => {
     return res.json("Funcionando")
