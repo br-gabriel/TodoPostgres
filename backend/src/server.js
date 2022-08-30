5b59c93dd34108c5876859b89caa1da0070778bf
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-08-30 15:57:11 -0300

    cors fixing

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/user.routes");
 const app = express();
 
 app.use(cors({
+    origin: "http://localhost:3000",
     credentials: true,
 }));
 app.use(express.json());
