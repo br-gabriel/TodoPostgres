9fe92ec816c33f0c23913656cf537165ae5a6840
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-09-02 19:07:41 -0300

    Private route update

--- frontend/todo-postgres/src/routes.js
+++ frontend/todo-postgres/src/routes.js
@@ -1,27 +1,29 @@
 import { 
     BrowserRouter as Router, 
-    Route, 
-    Routes 
+    Routes, 
+    Route
 } from "react-router-dom";
-
 import { Todos } from "./pages/Todos";
 import { SignUp } from "./pages/SignUp";
 import { SignIn } from "./pages/SignIn";
+import { AuthProvider } from "./contexts/authContext";
+import { PrivateRoute } from "./components/privateRoute";
 
 export function RoutesApp() {
-    // const Private = ({Item}) => {
-    //     const signed = true;
-    //     return signed > 0 ? <Item /> : <SignIn />;
-    // };
-
     return (
-        <Router>
-            <Routes>
-                <Route path="/todos" element={<Todos />} />
-                <Route exact path="/" element={<SignIn />} /> 
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="*" element={<SignIn />}/>
-            </Routes>
-        </Router>
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<SignIn />} /> 
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="*" element={<SignIn />}/>
+                    <Route path="/todos" element={
+                        <PrivateRoute>
+                            <Todos />
+                        </PrivateRoute>
+                    } />
+                </Routes>
+            </Router>
+        </AuthProvider>
     );
 };
