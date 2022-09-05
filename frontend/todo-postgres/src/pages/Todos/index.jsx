617f85b131e633701f7350d903b6df78e391c0c5
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-09-05 19:20:23 -0300

    footer added

--- frontend/todo-postgres/src/pages/Todos/index.jsx
+++ frontend/todo-postgres/src/pages/Todos/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "../../components/TodoComponents/Header";
 import { Main } from "../../components/TodoComponents/Main";
+import { Footer } from "../../components/TodoComponents/Footer";
  
 export function Todos() {
     return (
         <>
             <Header />
             <Main />
+            <Footer />
         </>
     );
 };
