01234b1eb68c86ec0a2dd86ba5641f5fdb7c15cd
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-09-14 19:10:25 -0300

    regex validation turned into a module

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -6,13 +6,12 @@ const prisma = new PrismaClient();
 
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-
+const emailRegex = require("../utils/regex");
 
 userRoutes.post("/user/signup", async (req, res) => {
     const { email, emailConfirm, password, passwordConfirm } = req.body;
 
     const checkPasswordLength = password.length;
-    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
     if (!email | !emailConfirm | !password | !passwordConfirm) {
         return res.status(400).json({ error: "Preencha todos os campos" });
