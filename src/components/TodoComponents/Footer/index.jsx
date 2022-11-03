c0c0c304884bd39442f40c492075e4a5d89c71c9
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-11-03 16:06:59 -0300

    Footer link update

--- src/components/TodoComponents/Footer/index.jsx
+++ src/components/TodoComponents/Footer/index.jsx
@@ -4,7 +4,7 @@ export function Footer() {
     return (
         <Container>
             <p>Made with ❤️ by
-                <a href="https://br.linkedin.com/in/gabriel-feitosa-3b9029193" target="_blank noreferrer">&nbsp;Gabriel Feitosa</a>
+                <a href="https://br.linkedin.com/in/gabriel-silva-feitosa" target="_blank noreferrer">&nbsp;Gabriel Feitosa</a>
             </p>
         </Container>
     );
