7766a61116eb9c88a9a46a60fc32051fcfb3e57b
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-28 16:57:40 -0300

    add request and response interceptors to API service

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,23 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
+
+api.interceptors.request.use(request => {
+    console.log('Request:', request);
+    return request;
+});
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        console.log('Response Error:', error.response);
+        return Promise.reject(error);
+    }
+);
 
 export default api;
