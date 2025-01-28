ff8a22a5edba5748ae70a6d80aad2694395f815a
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-28 17:29:00 -0300

    restore withCredentials option in API service configuration

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    // withCredentials: true,
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
     }
